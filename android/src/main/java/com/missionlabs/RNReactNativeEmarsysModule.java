d4964485275827064a7725fd1b31ae63d6909e6e
John Langan <john.langan@oxygenna.com>
2019-10-24 13:23:33 +0300

    update android

--- android/src/main/java/com/missionlabs/RNReactNativeEmarsysModule.java
+++ android/src/main/java/com/missionlabs/RNReactNativeEmarsysModule.java
@@ -1,18 +1,12 @@
 package com.missionlabs;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
-import com.emarsys.mobileengage.EventHandler;
-import com.emarsys.mobileengage.MobileEngageStatusListener;
-import com.emarsys.mobileengage.config.MobileEngageConfig;
-import com.emarsys.mobileengage.experimental.MobileEngageFeature;
-import com.emarsys.mobileengage.notification.NotificationEventHandler;
-import com.emarsys.mobileengage.MobileEngage;
+import com.emarsys.Emarsys;
+import com.emarsys.config.EmarsysConfig;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -21,61 +15,34 @@ import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.ReadableType;
 
-import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
 
-public class RNReactNativeEmarsysModule extends ReactContextBaseJavaModule implements EventHandler, NotificationEventHandler, MobileEngageStatusListener {
+public class RNReactNativeEmarsysModule extends ReactContextBaseJavaModule {
 
   private static final String TAG = "RNReactNativeEmarsys";
 
-  private final ReactApplicationContext reactContext;
-
   public RNReactNativeEmarsysModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    this.reactContext = reactContext;
     Log.i(TAG, "RNReactNativeEmarsysModule");
   }
 
   @ReactMethod
-  public void init(String applicationCode, String applicationPassword) {
-    Log.i(TAG, "init: " + applicationCode + "|" + applicationPassword);
-    Log.i(TAG, "Core version: " + com.emarsys.core.BuildConfig.VERSION_NAME);
-    Log.i(TAG, "MobileEngage version: " + com.emarsys.mobileengage.BuildConfig.VERSION_NAME);
-    MobileEngageConfig config = new MobileEngageConfig.Builder()
+  public void init(String applicationCode) {
+    Log.i(TAG, "init: " + applicationCode);
+
+    EmarsysConfig config = new EmarsysConfig.Builder()
       .application(getCurrentActivity().getApplication())
-      .credentials(applicationCode, applicationPassword)
-      .enableDefaultChannel("default", "here is a description")
-      .enableExperimentalFeatures(
-        MobileEngageFeature.IN_APP_MESSAGING,
-        MobileEngageFeature.USER_CENTRIC_INBOX
-      )
-      .statusListener(this)
-      .setDefaultInAppEventHandler(this)
-      .setNotificationEventHandler(this)
+      .mobileEngageApplicationCode(applicationCode)
+      .contactFieldId(12)
       .build();
-    MobileEngage.setup(config);
+    Emarsys.setup(config);
   }
 
   @ReactMethod
   public void setPushToken(String pushToken) {
     Log.i(TAG, "setPushToken");
-    MobileEngage.setPushToken(pushToken);
-  }
-
-  @ReactMethod
-  public void appLoginAnon() {
-    Log.i(TAG, "appLoginAnon");
-    String result = MobileEngage.appLogin();
-    Log.i(TAG, "appLoginAnon result = " + result);
-  }
-
-  @ReactMethod
-  public void appLoginUser(int contactFieldId, String contactFieldValue) {
-    Log.i(TAG, "appLoginUser");
-    String result = MobileEngage.appLogin(contactFieldId, contactFieldValue);
-    Log.i(TAG, "appLoginUser result = " + result);
+    Emarsys.getPush().setPushToken(pushToken);
   }
 
   @ReactMethod
@@ -94,28 +61,13 @@ public class RNReactNativeEmarsysModule extends ReactContextBaseJavaModule imple
           throw new IllegalArgumentException("Could not convert object with key: " + key + ".");
       }
     }
-    String result = MobileEngage.trackCustomEvent(eventName, eventAttributesMap);
-    Log.i(TAG, "trackCustomEvent result = " + result);
+    Emarsys.trackCustomEvent(eventName, eventAttributesMap);
   }
 
   @ReactMethod
   public void trackMessageOpen(Intent intent) {
     Log.i(TAG, "trackMessageOpen");
-    String result = MobileEngage.trackMessageOpen(intent);
-    Log.i(TAG, "trackMessageOpen result = " + result);
-  }
-
-  @ReactMethod
-  public void trackDeepLink(Activity activity, Intent intent) {
-    Log.i(TAG, "trackDeepLink");
-    MobileEngage.trackDeepLink(activity, intent);
-  }
-
-  @ReactMethod
-  public void appLogout() {
-    Log.i(TAG, "appLogout");
-    String result = MobileEngage.appLogout();
-    Log.i(TAG, "appLogout result = " + result);
+    Emarsys.getPush().trackMessageOpen(intent);
   }
 
   @Override
@@ -123,23 +75,23 @@ public class RNReactNativeEmarsysModule extends ReactContextBaseJavaModule imple
     return TAG;
   }
 
-  @Override
-  public void handleEvent(String eventName, JSONObject payload) {
-    Log.i(TAG, "handleEvent " + eventName);
-  }
-
-  @Override
-  public void handleEvent(Context context, String eventName, @Nullable JSONObject payload) {
-    Log.i(TAG, "handleEvent " + eventName);
-  }
-
-  @Override
-  public void onError(String id, Exception error) {
-    Log.i(TAG, "onError " + error.getMessage(), error);
-  }
-
-  @Override
-  public void onStatusLog(String id, String message) {
-    Log.i(TAG, "onStatusLog " + id + " " + message);
-  }
+//  @Override
+//  public void handleEvent(String eventName, JSONObject payload) {
+//    Log.i(TAG, "handleEvent " + eventName);
+//  }
+//
+//  @Override
+//  public void handleEvent(Context context, String eventName, @Nullable JSONObject payload) {
+//    Log.i(TAG, "handleEvent " + eventName);
+//  }
+//
+//  @Override
+//  public void onError(String id, Exception error) {
+//    Log.i(TAG, "onError " + error.getMessage(), error);
+//  }
+//
+//  @Override
+//  public void onStatusLog(String id, String message) {
+//    Log.i(TAG, "onStatusLog " + id + " " + message);
+//  }
 }
